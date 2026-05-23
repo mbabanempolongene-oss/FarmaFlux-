0805d1826f0aee0e25c99546a051b8bd37a214de
mbabanempolongene-oss <mbabanempolongene@gmail.com>
2026-05-23 02:15:07 +0200

    Create main.jsx
    Adiciona o main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import FarmaFlux from './farmaflux'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <FarmaFlux />
+  </React.StrictMode>
+)
